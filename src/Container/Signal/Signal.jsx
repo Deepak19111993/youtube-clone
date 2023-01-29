7810b7d719298bb6e6f0f43c37314a92ba37af4f
Deepak Kushwaha <deepak207510@gmail.com>
2023-01-29 23:22:33 +0530

    made the Counter

--- src/Container/Signal/Signal.jsx
+++ src/Container/Signal/Signal.jsx
@@ -4,64 +4,63 @@ const signalData = [
   {
     type: 'green',
     text: 'Green Text',
+    time: 10,
   },
   {
     type: 'red',
     text: 'red Text',
+    time: 10,
   },
   {
     type: 'orange',
     text: 'Orange Text',
+    time: 10,
   },
 ];
 
 const Signal = () => {
   const [data, setData] = useState(signalData);
-  const [time, setTime] = useState(10);
-  //   const [on, setOn] = useState(false);
-  const [typeStr] = useState(['green', 'red', 'orange']);
+  const [counter, setCounter] = useState('');
+  // const [stopCounter, setStopCounter] = useState(false);
 
   useEffect(() => {
-    setInterval(() => {
-      setTime((curr) => {
-        if (curr === 0) {
-          return 0;
+    timer();
+  }, [counter]);
+  const timer = () => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].type);
+      for (let j = 0; j < 10; j++) {
+        if (data[i].time > 0) {
+          const interval = setTimeout(() => {
+            setCounter(data[i].time);
+            data[i].time -= 1;
+          }, 1000);
+          return () => {
+            clearTimeout(interval);
+          };
         } else {
-          return curr - 1;
+          data[i].time = 10;
+          break;
         }
-      });
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    data.forEach((ele, index) => {
-      const x = typeStr[index];
-      console.log(typeStr[index], ele.type);
-      if (ele.type === x) {
-        setData({ ...ele, on: 'on' });
-      } else {
-        return ele;
       }
-    });
-  }, []);
+    }
+  };
 
-  console.log('on', data);
+  console.log('data', data);
 
   return (
     <div className='p-10 max-w-[500px] mx-auto'>
       <div className='flex gap-5 items-center'>
         <div className='flex flex-col gap-4'>
           {data.map((item) => {
-            let color = item.type;
+            let color = item?.type;
             return (
               <div
-                key={item.type}
+                key={item?.type}
                 className={`w-10 h-10 rounded-full border-2 transition-all flex items-center }`}
                 style={{ background: color, borderColor: color }}
               >
-                <span className='pl-[50px] whitespace-nowrap'>
-                  {item.text} {time}
-                </span>
+                <span className='pl-[50px] whitespace-nowrap'>{item.time}</span>
               </div>
             );
           })}
