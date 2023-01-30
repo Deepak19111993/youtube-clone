66435a769c3bfa668e872e125ddb4b73d40fa90a
Deepak Kushwaha <deepak.kushwaha@creolestudios.com>
2023-01-30 18:57:07 +0530

    make the traffic light

--- src/Container/Signal/Signal.jsx
+++ src/Container/Signal/Signal.jsx
@@ -20,31 +20,56 @@ const signalData = [
 
 const Signal = () => {
   const [data, setData] = useState(signalData);
-  const [counter, setCounter] = useState('');
-  // const [stopCounter, setStopCounter] = useState(false);
+  // const [counter, setCounter] = useState(10);
+
+  // useEffect(() => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     timer(i);
+  //   }
+  // }, []);
+
+  // const timer = (i) => {
+  //   const val = data[i];
+  //   let interval = setTimeout(() => {
+  //     for (let j = 10; j > 0; j--) {
+  //       if (counter > 0) {
+  //         setCounter(counter - 1);
+  //         console.log('val', val);
+  //         val.time = counter;
+  //         // console.log('index', val.type, j, counter);
+  //       } else if (counter === 0) {
+  //         setCounter(0);
+  //         clearTimeout(interval);
+  //       } else {
+  //         setCounter(10);
+  //       }
+  //     }
+  //   }, 1000);
+  // };
 
   useEffect(() => {
-    timer();
-  }, [counter]);
-  const timer = () => {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].type);
-      for (let j = 0; j < 10; j++) {
-        if (data[i].time > 0) {
-          const interval = setTimeout(() => {
-            setCounter(data[i].time);
-            data[i].time -= 1;
-          }, 1000);
-          return () => {
-            clearTimeout(interval);
-          };
-        } else {
-          data[i].time = 10;
-          break;
-        }
+    for (let i = 0; i <= 2; i++) {
+      if (data[i].time > 0) {
+        const interval = setTimeout(() => {
+          setData(
+            data.map((item, ind) =>
+              ind === i
+                ? { ...item, time: item.time - 1, on: 'on', off: '' }
+                : { ...item, time: item.time, off: 'off', on: '' }
+            )
+          );
+        }, 1000);
+
+        return () => {
+          clearTimeout(interval);
+        };
       }
     }
-  };
+
+    if (data[0].time === 0 && data[1].time === 0 && data[2].time === 0) {
+      setData(signalData);
+    }
+  }, [data]);
 
   console.log('data', data);
 
@@ -53,14 +78,19 @@ const Signal = () => {
       <div className='flex gap-5 items-center'>
         <div className='flex flex-col gap-4'>
           {data.map((item) => {
-            let color = item?.type;
+            // let color = item?.type;
             return (
               <div
                 key={item?.type}
                 className={`w-10 h-10 rounded-full border-2 transition-all flex items-center }`}
-                style={{ background: color, borderColor: color }}
+                style={{
+                  background: item.on ? 'green' : item.off ? 'red' : 'red',
+                  borderColor: item.on ? 'green' : item.off ? 'red' : 'red',
+                }}
               >
-                <span className='pl-[50px] whitespace-nowrap'>{item.time}</span>
+                <span className='pl-[50px] whitespace-nowrap'>
+                  {item.time ? item.time : 10}
+                </span>
               </div>
             );
           })}
